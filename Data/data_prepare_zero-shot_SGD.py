bb30677683abef84c5ca19b0c5cf8fbd84514204
김동하 <qeqw3435@naver.com>
2024-06-16 18:34:10 +0900

    Update data_prepare_zero-shot_SGD.py

--- Data/data_prepare_zero-shot_SGD.py
+++ Data/data_prepare_zero-shot_SGD.py
@@ -27,29 +27,14 @@ if __name__ == '__main__':
             dial_json_n, dial_idx, turn_idx, frame_idx, d_name, s_name = idx_list.split("|||") 
             
             dial_text = eval(test_data_lines[idx_].strip())
-            #print(dial_text['dialogue'])
             dial = dial_text['dialogue'].split(" [domain] ")
             input1 = ""
             input1 = input1 + dial[0] + "\n"
-            flag = 0
-            if "Possible Values" in dial[1]:
-                dial2 = dial[1].split(" [Possible Values] ")
-                
-                input1 = input1 + "[domain] " + dial2[0] + ". "
-                input1 = input1 + "This slot is categorical and you can only choose from the following available values: "
-                input1 = input1 + dial2[1] + ". "
                 
             else:
                 input1 = input1 + "[domain] " + dial[1] + ". "
                 flag = 1
-                
-            #print(dial)
-            input1 = input1 + "If the slot is not mentioned in the dialogue, just return NONE.\n"
-            input1 = input1 + "So the value of slot <"+ d_name+ "-" + s_name +"> is "
-            # print(input1)
-            # if flag == 1:
-            #     sys.exit(1)
-            
+
             output1 = dial_text['state']
             item['instruction'] = instru
             item['input'] = input1
